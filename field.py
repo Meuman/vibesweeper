55868956e96296739d5e14edf1c3d425becc263a
Meuman <44346724+Meuman@users.noreply.github.com>
2019-12-16 14:02:12 +0100

    Tweaked the docstrings and added some comments

--- field.py
+++ field.py
@@ -6,31 +6,34 @@ class Field:
     This is the field module
     It represents the field which is filled with mines, which the player has to avoid setting off.
     A bomb is represented by B.
-    An empty space is represented by N.
+    An empty space is represented by a 0 value.
+    Any other numerical value represents how many bombs are in the neighboring squares.
     """
     def __init__(self, columns, rows):
         """
         The constructor method takes 2 integers as its arguments, columns and rows.
         It makes a two dimensional list with using the arguments to determine the size of the dimensions.
-        It fills the list with the letter N, which represent an empty space.
+        It fills the list with the number 0, which represent an empty space.
         """
-        self.two_dim_field = numpy.full((columns, rows), 'N')
+        self.two_dim_field = numpy.full((columns, rows), '0')
 
-    def __ndarray__(self):
-        print(self.two_dim_field)
+    def __str__(self):
+        return str(self.two_dim_field)
 
     def fill_with_bombs(self, *coordinates):
         """
         This method takes the arguments (which are tuples) that represent coordinates (x, y)
         and fills these places with bombs (B)
+
         """
         for coordinate in coordinates:
             y = coordinate[0]-1  # arrays and lists start from 0 so subtract one
             x = coordinate[1]-1  # same here
             self.two_dim_field[x][y] = 'B'
+            # now we have to change the values of the neighboring squares if they are an int,
+
 
 
 field = Field(5, 5)
-# field.__ndarray__()
-field.fill_with_bombs((2, 5))
-field.__ndarray__()
+field.fill_with_bombs((2, 5), (2, 4), (3, 2), (1, 1))
+print(field)
