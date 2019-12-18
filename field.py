1bb0d5c3445a25f0f3c8c6a413a9b7ff20fd0525
Meuman <44346724+Meuman@users.noreply.github.com>
2019-12-18 17:09:14 +0100

    added a tile module and changed the design philosophy a little bit

--- field.py
+++ field.py
@@ -1,11 +1,30 @@
+from coords import get_neighbor_coords
+from tile import Tile
+
+
 class Field:
     def __init__(self, rows, columns):
         self.x_limit = columns
         self.y_limit = rows
-        self.two_dim_field = [[0 for y in range(rows)] for x in range(columns)]
+        self.two_dim_field = [[Tile(0, 'H') for y in range(rows)] for x in range(columns)]
 
     def show(self):
         for y in range(self.y_limit):
             for x in range(self.x_limit):
                 print(self.two_dim_field[x][y], end=' ')
             print()
+
+    def place_bombs(self, *coordinates):
+        for coordinate in coordinates:
+            x = coordinate[0]-1  # indexes start from 0, so we have to subtract 1
+            y = coordinate[1]-1
+            self.two_dim_field[x][y].set_value(9)
+            neighbor_coords = get_neighbor_coords(x, y)
+            for coords in neighbor_coords.values():
+                try:
+                    this_tile_value = self.two_dim_field[coords[0]][coords[1]].get_value()
+                    if this_tile_value != 9 and (0 <= coords[0] <= self.x_limit-1) and (0 <= coords[1] <= self.y_limit-1):
+                        self.two_dim_field[coords[0]][coords[1]] += 1
+                except Exception:  # not really sure how the fuck should i handle this exception or should i even handle it at all
+                    pass  # this try is really just here because the interpreter shits itself if it sees an index out of range
+                # also it's way easier to use try/except instead of writing a shitton of if statements (this comment was made by ask for forgiveness, not permission gang)
---
+++ tile.py
@@ -0,0 +1,30 @@
+class Tile:
+    def __init__(self, value, state):
+        self._value = value
+        self.state = state  # R - revealed, F - flag, H - hidden
+
+    def __add__(self, num):
+        self._value += num
+        return Tile(self._value, self.state)
+
+    def __str__(self):
+        if self.state == 'R':
+            return str(self._value)
+        else:
+            return str(self.state)
+
+    def reveal(self):
+        self.state = 'R'
+
+    def flag(self):
+        self.state = 'F'
+
+    def show(self):
+        if self.state == 'R':
+            return self._value
+
+    def set_value(self, value):
+        self._value = value
+
+    def get_value(self):
+        return self._value
