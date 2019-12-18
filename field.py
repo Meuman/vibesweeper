f1a37877018d353f230490b523afe7e571b59f24
Meuman <44346724+Meuman@users.noreply.github.com>
2019-12-18 12:37:16 +0100

    minor tweaks

--- field.py
+++ field.py
@@ -1,8 +1,8 @@
 class Field:
-    def __init__(self, columns, rows):
-        self.y_limit = columns
-        self.x_limit = rows
-        self.two_dim_field = [[0 for x in range(columns)] for y in range(rows)]
+    def __init__(self, rows, columns):
+        self.x_limit = columns
+        self.y_limit = rows
+        self.two_dim_field = [[0 for x in range(rows)] for y in range(columns)]
 
     def show(self):
         for x in range(self.y_limit):
@@ -16,40 +16,40 @@ class Field:
             y = coordinate[1]-1  # same here
             self.two_dim_field[x][y] = 9
             # now we have to change the values of the neighboring squares if they aren't a mine
-            if (y > 0):  # case                             top
+            if (y > 0):  # top
                 a = self.two_dim_field[x][y-1]
                 if a != 9:
                     self.two_dim_field[x][y-1] += 1
-            if (y < self.y_limit - 1):  # case              down
+            if (y < self.y_limit - 1):  # down
                 a = self.two_dim_field[x][y+1]
                 if a != 9:
                     self.two_dim_field[x][y+1] += 1
-            if (x > 0):  # case                             left
+            if (x > 0):  # left
                 a = self.two_dim_field[x-1][y]
                 if a != 9:
                     self.two_dim_field[x-1][y] += 1
-            if (x < self.x_limit - 1):  # case               right
+            if (x < self.x_limit - 1):  # right
                 a = self.two_dim_field[x+1][y]
                 if a != 9:
                     self.two_dim_field[x+1][y] += 1
-            if (y > 0 and x > 0):  # case                     top left
+            if (y > 0 and x > 0):  # top left
                 a = self.two_dim_field[x-1][y-1]
                 if a != 9:
                     self.two_dim_field[x-1][y-1] += 1
-            if (y > 0 and x < self.x_limit - 1):  # case     top right
+            if (y > 0 and x < self.x_limit - 1):  # top right
                 a = self.two_dim_field[x+1][y-1]
                 if a != 9:
                     self.two_dim_field[x+1][y-1] += 1
-            if (y < self.y_limit - 1 and x > 0):  # case     bot left
+            if (y < self.y_limit - 1 and x > 0):  # bot left
                 a = self.two_dim_field[x-1][y+1]
                 if a != 9:
                     self.two_dim_field[x-1][y+1] += 1
-            if (y < self.y_limit - 1 and x < self.x_limit - 1):
+            if (y < self.y_limit - 1 and x < self.x_limit - 1):  # bot right
                 a = self.two_dim_field[x+1][y+1]
                 if a != 9:
                     self.two_dim_field[x+1][y+1] += 1
 
 
 field = Field(5, 5)
-field.fill_with_bombs((2, 4), (3, 4))
+field.fill_with_bombs((5, 3))
 print(field.show())
