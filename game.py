8698b48355a5d1e63ff40787b37b8d8670af6949
Meuman <44346724+Meuman@users.noreply.github.com>
2020-01-11 23:47:28 +0100

    maked the vibe check emoji scalable and more

--- game.py
+++ game.py
@@ -9,10 +9,11 @@ class Game:
         icon = pg.image.load('C:\\Users\\derpi\\Documents\\GitHub\\minesweeper\\ico.png')
         pg.display.set_icon(icon)
         pg.display.set_caption("Vibesweeper")
-        self.RECT_SIZE = 75
+        self.RECT_SIZE = 250
         self.field = field
         self.result = None
         self.init_screen()
+        self.draw_tiles()
         self.draw_borders()
         self.init_menu()
         pg.display.update()
@@ -28,18 +29,31 @@ class Game:
         self.DISPLAY = pg.display.set_mode((self.display_width, self.display_height))
 
     def init_menu(self):
-        self.menurect = pg.Rect(0, 0, self.display_width, self.RECT_SIZE)
-        pg.draw.rect(self.DISPLAY, (200, 200, 200), self.menurect)
+        menurect = pg.Rect(0, 0, self.display_width, self.RECT_SIZE)
+        pg.draw.rect(self.DISPLAY, (200, 200, 200), menurect)
+        vibe_man = pg.image.load('C:\\Users\\derpi\\Documents\\GitHub\\minesweeper\\him.png')
+        vibe_man = pg.transform.smoothscale(vibe_man, (self.RECT_SIZE, self.RECT_SIZE))
+        self.DISPLAY.blit(vibe_man, (self.display_width//2 - self.RECT_SIZE//2, 0))
 
     def draw_borders(self):
         horizontal_range = self.field.y_limit
         vertical_range = self.field.x_limit
         for horizontal_iterator in range(horizontal_range):
-            horizontal_rect = pg.Rect(0, (horizontal_iterator+2)*75, self.display_width, 2)
-            pg.draw.rect(self.DISPLAY, (255, 0, 0), horizontal_rect)
+            horizontal_rect = pg.Rect(0, (horizontal_iterator+1)*self.RECT_SIZE, self.display_width, 1)
+            pg.draw.rect(self.DISPLAY, (0, 0, 0), horizontal_rect)
         for vertical_iterator in range(vertical_range):
-            vertical_rect = pg.Rect((vertical_iterator+1)*75, 0, 2, self.display_height)
-            pg.draw.rect(self.DISPLAY, (255, 0, 0), vertical_rect)
+            vertical_rect = pg.Rect((vertical_iterator+1)*self.RECT_SIZE, 0, 1, self.display_height)
+            pg.draw.rect(self.DISPLAY, (0, 0, 0), vertical_rect)
+
+    def draw_tiles(self):
+        field = self.field.two_dim_field
+        for rows in range(self.field.y_limit):
+            for columns in range(self.field.x_limit):
+                rect_to_draw = pg.Rect(columns*(self.RECT_SIZE), self.RECT_SIZE + rows*(self.RECT_SIZE), self.RECT_SIZE, self.RECT_SIZE)
+                if field[columns][rows].state == 'H':
+                    pg.draw.rect(self.DISPLAY, (175, 175, 175), rect_to_draw)
+                if field[columns][rows].state == 'R':
+                    pg.draw.rect(self.DISPLAY, (125, 125, 125), rect_to_draw)
 
     def click_tile(self, x, y):
         x -= 1
@@ -48,7 +62,9 @@ class Game:
             self.field.two_dim_field[x][y].reveal()
             if self.field.two_dim_field[x][y].get_value() == 9:
                 self.result = False
-            self.reveal_surrounding_tiles(x, y)
+                return
+            if self.field.two_dim_field[x][y].get_value() == 0:
+                self.reveal_surrounding_tiles(x, y)
         except Exception:
             pass
 
@@ -59,16 +75,29 @@ class Game:
         for coords in neighbor_coords.values():
             try:
                 processed_tile = self.field.two_dim_field[coords[0]][coords[1]]
-                if processed_tile.state == 'H':
+                if processed_tile == 'H':
                     if processed_tile.get_value() != 9:
-                        processed_tile.reveal()
-                    if processed_tile.get_value() == 0:
-                        self.reveal_surrounding_tiles(coords[0], coords[1])
+                        self.click_tile(coords[0], coords[1])
             except Exception:
                 pass
 
+    # def reveal_surrounding_tiles(self, x, y):
+    #     x -= 1
+    #     y -= 1
+    #     neighbor_coords = get_neighbor_coords(x, y)
+    #     for coords in neighbor_coords.values():
+    #         try:
+    #             processed_tile = self.field.two_dim_field[coords[0]][coords[1]]
+    #             if processed_tile.state == 'H':
+    #                 if processed_tile.get_value() != 9:
+    #                     processed_tile.reveal()
+    #                 if processed_tile.get_value() == 0:
+    #                     self.reveal_surrounding_tiles(coords[0], coords[1])
+    #         except Exception:
+    #             pass
 
-field = f.Field(3, 7)
+
+field = f.Field(6, 6)
 game = Game(field)
 
 # field = f.Field(5, 5)
